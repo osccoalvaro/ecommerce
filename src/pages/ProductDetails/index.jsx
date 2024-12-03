328c0516640238a629d7f211e2b3ab3e3d772b46
osccoalvaro <osccoalvaro@gmail.com>
2024-12-02 22:12:49 -0500

    add button whatsapp

--- src/pages/ProductDetails/index.jsx
+++ src/pages/ProductDetails/index.jsx
@@ -20,6 +20,10 @@ const ProductDetails = () => {
     );
   }
   const { title, price, description, image } = product;
+
+  const whatsappNumber = "51987654321"; // Reemplaza con tu número de WhatsApp
+  const whatsappMessage = `Hola, estoy interesado en el producto ${title}. ¿Podrías brindarme más información?`;
+
   return (
     <section className="pt-32 pb-12 lg:py-32 flex items-center">
       <div className="container mx-auto">
@@ -57,12 +61,43 @@ const ProductDetails = () => {
               ))}
             </p>
 
-            <button
-              className="mb-3 bg-primary py-4 px-8 text-white text-base"
-              onClick={() => addToCart(product, product.id)}
-            >
-              Agregar al carrito
-            </button>
+            <div className="flex flex-col md:flex-row items-center gap-4">
+  <button
+    className="bg-primary py-4 px-8 text-white text-base md:mr-10"
+    onClick={() => addToCart(product, product.id)}
+  >
+    Agregar al carrito
+  </button>
+  <a
+    href={`https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
+      whatsappMessage
+    )}`}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="bg-green-500 pr-8 pl-16 py-3 text-white text-base flex items-center gap-4 rounded-full shadow-md hover:shadow-lg transition-shadow duration-300 relative"
+  >
+    <div className="absolute -left-6">
+      <img
+        src="/img/avatar4.jpg" // Ruta del avatar
+        alt="Asesor Comercial"
+        className="h-20 rounded-full border-2 border-white shadow-md"
+      />
+    </div>
+    <div className="flex flex-col">
+      <span className="font-semibold text-sm">
+        Asesor Comercial 
+        <span className="bg-white text-green-500 rounded-full text-xs px-2 py-[2px] ml-1">
+          Online
+        </span>
+      </span>
+      <span className="text-sm font-medium">Información vía WhatsApp</span>
+    </div>
+  </a>
+</div>
+
+
+
+        
           </div>
         </div>
       </div>
